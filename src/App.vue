8cdbad9e79cde2b7176583ada9d5b8b0f54e43d2
Fernando Bordin <fernandobordin07@hotmail.com>
2024-08-26 21:06:54 -0300

    componetizando a aplicacao

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { reactive } from 'vue';
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
+
+  const estado = reactive({
+    filtro: 'todas' ,
+    tarefaTemp: '',
+    Tarefa: [
+      {
+        titulo: 'Estudatr ES6',
+        finalizada: false,
+      },
+      {
+        titulo: 'Estudar SASS',
+        finalizada: false,
+      },
+      {
+        titulo: 'Ir para academia',
+        finalizada: true,
+      }
+    ]
+  })
+
+  const getTarefasPendentes = () => {
+    return estado.Tarefa.filter(tarefa => !tarefa.finalizada)
+  }
+
+  const getTarefasFinalizadas = () => {
+    return estado.Tarefa.filter(tarefa => tarefa.finalizada)
+  }
+
+  const getTarefasFiltradas = () => {
+    const {filtro} = estado;
+
+    switch (filtro) {
+      case 'pendentes':
+        return getTarefasPendentes();
+      case 'finalizadas':
+        return getTarefasFinalizadas();
+      default:
+        return estado.Tarefa;
+    }
+  }
+
+  const cadastrarTarefa = () => {
+    const tarefaNova = {
+      titulo: estado.tarefaTemp,
+      finalizada: false,
+    }
+    estado.Tarefa.push(tarefaNova);
+    estado.tarefaTemp = '';
+  }
+</script>
+
+<template>
+  <div class="container"> 
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length" />
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastrar-tarefa="cadastrarTarefa"/>
+    <ListaDeTarefas :tarefas="getTarefasFiltradas()" /> 
+  </div>
+</template>
+
+
---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['cadastrarTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp'])
+</script>
+<template>
+    <form @submit.prevent="props.cadastrarTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required
+                    class="form-control" type="text" placeholder="Digite aqui a descricao da tarefa">
+            </div>
+            <div class="col-md-1">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                    <option value="todas">Todas Tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select> 
+            </div>
+        </div>
+    </form>
+</template>
